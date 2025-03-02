9b784c22327028748b4e4fa4e9923c9e2161dc42
Saikatcoder <saikat2018dutta18@gmail.com>
2025-03-02 10:35:28 +0530

    create server

--- Day-2/index.js
+++ Day-2/index.js
@@ -1,1 +1,12 @@
-console.log("hello");
+const http = require('http');
+
+const server = http.createServer((req, res)=>{
+    res.end("Hello i am server ✅")
+})
+
+const PORT = 3000;
+
+server.listen(PORT, ()=>{
+    console.log(`server is connected on port number ${PORT}`);
+    
+})
