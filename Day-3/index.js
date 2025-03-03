a362dbb4353efe64bbbf9a1fab57b6413dd8e010
Saikatcoder <saikat2018dutta18@gmail.com>
2025-03-03 11:55:14 +0530

    user data create

--- Day-3/index.js
+++ Day-3/index.js
@@ -1,9 +1,17 @@
 import express from 'express';
-
+import data from "./data/data.js";
 const app = express();
 
 const PORT = 8000;
+app.get('/',(req,res)=>{
+    res.status(200).send("hello sir🙊")
+})
 
+// Industry standards
+app.get("api/v1/users",(req,res)=>{
+    res.status(200).send(data)
+})
 app.listen(PORT, (req,res)=>{
     console.log(`Server is running on port ${PORT}`);
-})
+});
+
