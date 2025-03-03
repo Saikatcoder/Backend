603760000c71e238d6e5a4aeb642c1d9cd923627
Saikatcoder <saikat2018dutta18@gmail.com>
2025-03-03 12:35:17 +0530

    api route create

--- Day-3/data/data.js
+++ Day-3/data/data.js
@@ -1,11 +1,10 @@
-  const data = {
+  const data = [
    // lets create a 5 user o= data obj with{id,name,displayname,email,password}
-    users: [
         {id: 1, name: 'John Doe', displayname: 'John Doe', email: 'johndoe@example.com', password: 'password123'},
         {id: 2, name: 'Jane Doe', displayname: 'Jane Doe', email: 'janedoe@example.com', password: 'password456'},
         {id: 3, name: 'Alice Smith', displayname: 'Alice Smith', email: 'alicesmith@example.com', password: 'password789'},
         {id: 4, name: 'Bob Johnson', displayname: 'Bob Johnson', email: 'bobjohnson@example.com', password: 'password012'},
-        {id: 5, name: 'Emily Davis', displayname: 'Emily Davis', email: 'emilydavis@example.com', password: 'password222'},] 
-}
+        {id: 5, name: 'Emily Davis', displayname: 'Emily Davis', email: 'emilydavis@example.com', password: 'password222'}
+  ]
 
-export default data;
+export default data
--- Day-3/index.js
+++ Day-3/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import data from "./data/data.js";
+import Userdata from "./data/data.js";
 const app = express();
 
 const PORT = 8000;
@@ -7,10 +7,30 @@ app.get("/",(req,res)=>{
     res.status(200).send("hello sir🙊")
 })
 
-// Industry standards
-app.get("/api/v1/users",(req,res)=>{
-    res.status(200).send(data)
+// // Industry standards
+// app.get("/api/v1/users",(req,res)=>{
+//     const name = req.query;
+//     if(name){
+//         const users = data.filter((users)=>{
+//             return users.name === name;
+//         })
+//         res.status(200).send(users)
+//     }
+//     req.status(200).send(data)
+// })
+
+app.get("/api/v1/users/:id",(req,res)=>{
+    const {id} = req.params;
+
+    const porsedId = parseInt(id);
+
+    const user = Userdata.find((user)=>{
+        return user.id === porsedId;
+    })
+    res.status(200).send(user);
 })
+
+
 app.listen(PORT, (req,res)=>{
     console.log(`Server is running on port ${PORT}`);
 });
